769468e0eebe890ff4ba1c3ce9b39607ff86ad47
mohamed benchrifa <medmohamed.9@outlook.com>
2021-04-19 18:03:58 +0100

    Added pronunciation support

--- Kawazu/Division.cs
+++ Kawazu/Division.cs
@@ -38,10 +38,24 @@ namespace Kawazu
                 return builder.ToString();
             }
         }
+        public string HiraPronunciation
+        {
+            get
+            {
+                var builder = new StringBuilder();
+                foreach (var element in this)
+                {
+                    builder.Append(element.HiraPronunciation);
+                }
+
+                return builder.ToString();
+            }
+        }
 
         public string KataReading => Utilities.ToRawKatakana(HiraReading);
-
+        public string KataPronunciation => Utilities.ToRawKatakana(HiraPronunciation);
         public string RomaReading => Utilities.ToRawRomaji(HiraReading);
+        public string RomaPronunciation => Utilities.ToRawRomaji(HiraPronunciation);
 
         public readonly bool IsEndsInTsu;
 
@@ -52,23 +66,23 @@ namespace Kawazu
             switch (type)
             {
                 case TextType.PureKana:
-                    foreach (var ch in node.Surface)
-                    {
-                        this.Add(new JapaneseElement(ch.ToString(), Utilities.ToRawKatakana(ch.ToString()), TextType.PureKana, system));
-                    }
+                    for(int i = 0; i < node.Surface.Length; i++)
+                        this.Add(new JapaneseElement(node.Surface[i].ToString(), Utilities.ToRawKatakana(node.Surface[i].ToString()), node.Pronounciation[i].ToString(), TextType.PureKana, system));
                     break;
                 case TextType.PureKanji:
-                    this.Add(new JapaneseElement(node.Surface, node.Reading, TextType.PureKanji, system));
+                    this.Add(new JapaneseElement(node.Surface, node.Reading, node.Pronounciation, TextType.PureKanji, system));
                     break;
                 case TextType.KanjiKanaMixed:
                     var surfaceBuilder = new StringBuilder(node.Surface);
                     var readingBuilder = new StringBuilder(node.Reading);
+                    var pronounciationBuilder = new StringBuilder(node.Pronounciation);
                     var kanasInTheEnd = new StringBuilder();
                     while (Utilities.IsKana(surfaceBuilder[0])) // Pop the kanas in the front.
                     {
-                        this.Add(new JapaneseElement(surfaceBuilder[0].ToString(), Utilities.ToRawKatakana(surfaceBuilder[0].ToString()), TextType.PureKana, system));
+                        this.Add(new JapaneseElement(surfaceBuilder[0].ToString(), Utilities.ToRawKatakana(surfaceBuilder[0].ToString()), pronounciationBuilder[0].ToString(), TextType.PureKana, system));
                         surfaceBuilder.Remove(0, 1);
                         readingBuilder.Remove(0, 1);
+                        pronounciationBuilder.Remove(0, 1);
                     }
                     
                     while (Utilities.IsKana(surfaceBuilder[surfaceBuilder.Length - 1])) // Pop the kanas in the end.
@@ -76,6 +90,7 @@ namespace Kawazu
                         kanasInTheEnd.Append(surfaceBuilder[surfaceBuilder.Length - 1].ToString());
                         surfaceBuilder.Remove(surfaceBuilder.Length - 1, 1);
                         readingBuilder.Remove(readingBuilder.Length - 1, 1);
+                        pronounciationBuilder.Remove(pronounciationBuilder.Length - 1, 1);
                     }
 
                     if (Utilities.HasKana(surfaceBuilder.ToString())) // For the middle part:
@@ -88,47 +103,51 @@ namespace Kawazu
                             select ele;
                         
                         var kanaList = kanas.ToList();
-                        
-                        foreach (var ch in surfaceBuilder.ToString())
+
+                        var ch = surfaceBuilder.ToString();
+                        for(int i = 0; i < ch.Length; i++)
                         {
-                            if (Utilities.IsKanji(ch))
+                            if (Utilities.IsKanji(ch[i]))
                             {
                                 if (kanaIndex >= kanaList.Count)
                                 {
-                                    this.Add(new JapaneseElement(ch.ToString(), readingBuilder.ToString(previousIndex + 1, readingBuilder.Length - previousIndex - 1), TextType.PureKanji, system));
+                                    this.Add(new JapaneseElement(ch[i].ToString(), readingBuilder.ToString(previousIndex + 1, readingBuilder.Length - previousIndex - 1), pronounciationBuilder.ToString(previousIndex + 1, readingBuilder.Length - previousIndex - 1), TextType.PureKanji, system));
                                     continue;
                                 }
 
                                 var index = readingBuilder.ToString()
                                     .IndexOf(Utilities.ToRawKatakana(kanaList[kanaIndex].ToString()), StringComparison.Ordinal);
 
-                                this.Add(new JapaneseElement(ch.ToString(), readingBuilder.ToString(previousIndex + 1, index - previousIndex - 1), TextType.PureKanji, system));
+                                this.Add(new JapaneseElement(ch[i].ToString(), readingBuilder.ToString(previousIndex + 1, index - previousIndex - 1), pronounciationBuilder.ToString(previousIndex + 1, index - previousIndex - 1), TextType.PureKanji, system));
                                 previousIndex = index;
                                 kanaIndex++;
                             }
 
-                            if (Utilities.IsKana(ch))
+                            if (Utilities.IsKana(ch[i]))
                             {
-                                this.Add(new JapaneseElement(ch.ToString(), Utilities.ToRawHiragana(ch.ToString()), TextType.PureKana, system));
+                                var kana = Utilities.ToRawKatakana(ch[i].ToString());
+                                this.Add(new JapaneseElement(ch[i].ToString(), kana, kana, TextType.PureKana, system));
                             }
+                        
                         }
                     }
 
                     else
                     {
-                        this.Add(new JapaneseElement(surfaceBuilder.ToString(), readingBuilder.ToString(), TextType.PureKanji, system));
+                        this.Add(new JapaneseElement(surfaceBuilder.ToString(), readingBuilder.ToString(), pronounciationBuilder.ToString(), TextType.PureKanji, system));
                     }
 
                     if (kanasInTheEnd.Length != 0)
                     {
                         for (var i = kanasInTheEnd.Length - 1; i >= 0; i--)
                         {
-                            this.Add(new JapaneseElement(kanasInTheEnd.ToString()[i].ToString(), Utilities.ToRawKatakana(kanasInTheEnd.ToString()[i].ToString()), TextType.PureKana, system));
+                            var kana = Utilities.ToRawKatakana(kanasInTheEnd.ToString()[i].ToString());
+                            this.Add(new JapaneseElement(kanasInTheEnd.ToString()[i].ToString(), kana, kana, TextType.PureKana, system));
                         }
                     }
                     break;
                 case TextType.Others:
-                    this.Add(new JapaneseElement(node.Surface, node.Surface, TextType.Others, system));
+                    this.Add(new JapaneseElement(node.Surface, node.Surface, node.Pronounciation, TextType.Others, system));
                     break;
             }
         }
